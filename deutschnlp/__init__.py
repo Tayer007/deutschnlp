a21c67e354550c5863a387597f23c358f4688e06
Fourat Garrach <34721334+Tayer007@users.noreply.github.com>
2025-03-29 01:57:22 +0100

    Update __init__.py

---
+++ deutschnlp/__init__.py
@@ -0,0 +1,10 @@
+"""
+DeutschNLP - A comprehensive toolkit for analyzing German text
+"""
+
+__version__ = "0.1.0"
+
+from deutschnlp.analyzer import GermanAnalyzer, GermanAnalysisResult
+from deutschnlp.sentiment import SentimentAnalyzer
+from deutschnlp.similarity import TextComparator, SimilarityResult
+from deutschnlp.visualizer import DependencyVisualizer
